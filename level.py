a7d41d726b3c5f9755be3b01b9f5591449c6b1be
Austin Burke <austinlayneburke@gmail.com>
2023-02-21 07:26:08 -0700

    Add player movement and collision

--- level.py
+++ level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-
+from debug import debug
 class Level:
     def __init__(self) -> None:
 
@@ -24,8 +24,10 @@ class Level:
                 if col == 'x':
                     Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
                 if col == 'p':
-                    Player((x,y), [self.visible_sprites])
+                    self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
     
     def run(self):
         # update and draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
