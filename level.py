3d3cc817d149e65fede0104dfc6e088a9e7e97e9
Austin Burke <austinlayneburke@gmail.com>
2023-02-28 06:14:20 -0700

    Implement floor

--- level.py
+++ level.py
@@ -41,7 +41,7 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2()
 
         # creating the floor
-        self.floor_surface = pygame.image.load('./graphics/tilemap/ground.png').convert()
+        self.floor_surf = pygame.image.load('./graphics/tilemap/ground.png').convert()
         self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
 
     def custom_draw(self, player):
@@ -49,6 +49,10 @@ class YSortCameraGroup(pygame.sprite.Group):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
