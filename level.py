eff4ea12844e0e6bb2da80c446c8f9493b50148b
Austin Burke <austinlayneburke@gmail.com>
2023-02-28 06:12:04 -0700

    Import floor asset

--- level.py
+++ level.py
@@ -39,7 +39,11 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
-    
+
+        # creating the floor
+        self.floor_surface = pygame.image.load('./graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
 
         # getting the offset
