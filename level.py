debdff24b3517a3761e39f733a35dcda17c7b03a
Austin Burke <austinlayneburke@gmail.com>
2023-03-22 06:16:10 -0600

    Add initial pause mechanic with black screen

--- level.py
+++ level.py
@@ -10,6 +10,7 @@ from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer 
 from magic import MagicPlayer
+from upgrade import Upgrade
 
 
 class Level:
@@ -17,6 +18,7 @@ class Level:
 
         # get the display surface 
         self.display_surface = pygame.display.get_surface()
+        self.game_paused = False
 
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
@@ -32,6 +34,7 @@ class Level:
 
         # user interface
         self.ui = UI()
+        self.upgrade = Upgrade(self.player)
 
         # particles
         self.animation_player = AnimationPlayer()
@@ -140,14 +143,21 @@ class Level:
 
     def add_exp(self, amount):
         self.player.exp += amount
+    
+    def toggle_menu(self):
+        self.game_paused = not self.game_paused
 
     def run(self):
-        # update and draw the game
         self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        self.visible_sprites.enemy_update(self.player)
-        self.player_attack_logic()
         self.ui.display(self.player)
+
+        if self.game_paused:
+            # display upgrade menu
+            self.upgrade.display()
+        else:
+            self.visible_sprites.update()
+            self.visible_sprites.enemy_update(self.player)
+            self.player_attack_logic()
 
 
 class YSortCameraGroup(pygame.sprite.Group):
